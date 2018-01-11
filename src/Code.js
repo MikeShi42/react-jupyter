22dd4582cd631d3531d69b5398fd2e150542c138
Mike Shi <michaelshi42@gmail.com>
2018-01-10 23:09:28 -0800

    Use Prism for syntax highlighting

--- src/Code.js
+++ src/Code.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react'
 import PropTypes from 'prop-types';
-import highlight from 'highlight.js'
-import 'highlight.js/styles/github.css'
+import PrismCode from 'react-prism';
 
 class Code extends Component {
   static propTypes = {
@@ -11,32 +10,15 @@ class Code extends Component {
     showCode: PropTypes.bool
   }
 
-  componentDidMount() {
-    const { showCode } = this.props
-    if (!showCode) return
-
-    highlight.highlightBlock(this._elem)
-  }
-
-  componentDidUpdate() {
-    const { showCode } = this.props
-    if (!showCode) return
-
-    highlight.initHighlighting.called = false
-    highlight.highlightBlock(this._elem)
-  }
-
   render() {
     const { string, className, language = '', showCode } = this.props
 
     if (!showCode) return <div></div>
 
     return (
-      <pre className={className} data-language={language}>
-        <code ref={c => {this._elem = c}}>
-          {string}
-        </code>
-      </pre>
+      <PrismCode component="pre" className={className} data-language={language}>
+        {string}
+      </PrismCode>
     )
   }
 }
